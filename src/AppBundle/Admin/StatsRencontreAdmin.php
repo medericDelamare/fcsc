898801db0e0a0c28bd01e2ba5e7b16a0f5dc6225
mdelamare <mederic.delamare@trsb.net>
2018-12-04 16:17:40 +0100

    feat(admin): filtre sur les rencontres du club pour ajouter une stat rencontre

--- src/AppBundle/Admin/StatsRencontreAdmin.php
+++ src/AppBundle/Admin/StatsRencontreAdmin.php
@@ -3,7 +3,11 @@
 namespace AppBundle\Admin;
 
 
+use AppBundle\Entity\Club;
+use AppBundle\Entity\Equipe;
+use AppBundle\Entity\Rencontre;
 use AppBundle\Entity\StatsRencontre;
+use AppBundle\Repository\RencontreRepository;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
@@ -14,7 +18,16 @@ class StatsRencontreAdmin extends AbstractAdmin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('rencontre')
+            ->add('rencontre', EntityType::class, [
+                'class' => Rencontre::class,
+                'query_builder' => function(RencontreRepository $er) {
+                    return $er->createQueryBuilder('r')
+                        ->join(Equipe::class, 'ed', 'WITH', 'ed.id = r.equipeDomicile')
+                        ->join(Equipe::class, 'ee', 'WITH', 'ee.id = r.equipeExterieure')
+                        ->where('ed.club = 1')
+                        ->orWhere('ee.club = 1');
+                }
+            ])
             ->add('joueurs')
             ->add('cartonsJaunes')
             ->add('cartonsRouges')
