9fc5ec89c90f5e9b38c4a0a5bed20da5823c4f6e
Unknown <delamare.mederic@gmail.com>
2019-01-15 13:14:41 +0100

    feat(stages): inscriptions aux stages

---
+++ src/AppBundle/Controller/StageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\StagePdf;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class StageController extends Controller
+{
+    /**
+     * @Route("/stages", name="stages")
+     * @Template()
+     */
+    public function listAction()
+    {
+        $stages = $this->getDoctrine()->getManager()->getRepository(StagePdf::class)->findAll();
+
+
+
+        // replace this example code with whatever you need
+        return $this->render(':default:stages.html.twig', [
+            'stages' => $stages
+        ]);
+    }
+}
