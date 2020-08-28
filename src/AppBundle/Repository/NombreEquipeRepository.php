44e938a31e00cc6a87a69796a21e4238594bac9e
mdelamare <delamare.mederic@gmail.com>
2020-08-28 17:34:40 +0200

    feat(nb-equipes): Administration et renvoi des informations

--- src/AppBundle/Repository/NombreEquipeRepository.php
+++ src/AppBundle/Repository/NombreEquipeRepository.php
@@ -8,5 +8,10 @@ use Doctrine\ORM\EntityRepository;
 
 class NombreEquipeRepository extends EntityRepository
 {
-
+    public function findAllOrdered(){
+        return $this->createQueryBuilder('nb')
+            ->orderBy('nb.ordre')
+            ->getQuery()
+            ->getResult();
+    }
 }
